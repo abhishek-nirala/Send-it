813cde7531968a4406d57bfc8f6b242cddcdd318
abhishek-nirala <doctordeath918@gmail.com>
2025-01-10 17:18:33 +0530

    logic for signin is written

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,8 +1,131 @@
+'use client'
+import { toast } from '@/hooks/use-toast'
+// import { ApiResponses } from '@/types/ApiResponses'
+import { zodResolver } from '@hookform/resolvers/zod'
+// import axios, { AxiosError } from 'axios'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from '@/components/ui/input'
+import { Loader2 } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { singInSchema } from '@/schemas/signInSchema'
 import React from 'react'
-
+import { signIn } from 'next-auth/react'
 const SignIn = () => {
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof singInSchema>>({
+    resolver: zodResolver(singInSchema),
+    defaultValues: {
+      email: '',
+      password: ''
+    },
+  })
+
+  const onSubmit = async (data: z.infer<typeof singInSchema>) => {
+    setIsSubmitting(true)
+    try {
+      const result = await signIn('credentials', {
+        redirect: false,
+        email: data.email,
+        password: data.password
+      })
+      console.log("signIn result : ", result)
+      if (result?.error) {
+        toast({
+          title: 'Login Failed',
+          description: result.error,
+          variant: 'destructive'
+        })
+      }
+      console.log("result.error : ", result?.error)
+      if (result?.url) {
+        router.push('/dashboard')
+      }
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: 'Status:500',
+        description: 'Internal Error',
+        variant: 'destructive'
+      })
+    }finally{
+      setIsSubmitting(false)
+    }
+
+  }
+
   return (
-    <div>SignIn</div>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-200">
+        <div className="w-full max-w-md text-center bg-white border border-gray-200 rounded-lg shadow-md p-8">
+          <h1 className="text-4xl font-bold mb-2 bg-gradient-to-r from-purple-400 to-pink-600 text-transparent bg-clip-text">
+            Welcome Back
+          </h1>
+          {/* <p className="text-lg text-gray-600 mb-8 mt-5">Anonymous Feedback </p> */}
+          <h2 className="text-2xl font-bold mb-6">Sign In</h2>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+
+              <FormField
+                name="email"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem className="text-left space-y-2">
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input type="email" placeholder="example001@email.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name="password"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem className="text-left space-y-2">
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="!@#$%^&*dip123"
+                        {...field}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" disabled={isSubmitting}>
+                {
+                  isSubmitting ?
+                    <>
+                      <Loader2 className="animate-spin" />
+                    </>
+                    :
+                    "Sign In"
+                }
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </main>
+
+    </>
   )
 }
 
